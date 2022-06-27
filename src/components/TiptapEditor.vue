7d1af330e5a8d6ce59179bfe3e379cb3f2bee929
David R. White <david.r.white@sheffield.ac.uk>
2022-06-27 10:58:53 +0100

    Basic TipTap Editor

---
+++ src/components/TiptapEditor.vue
@@ -0,0 +1,33 @@
+<template>
+    <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+    components: {
+        EditorContent,
+    },
+
+    data() {
+        return {
+            editor: null,
+        }
+    },
+
+    mounted() {
+        this.editor = new Editor({
+            content: '<p>This text is inside a basic TipTape Editor. 🎉</p>',
+            extensions: [
+                StarterKit,
+            ],
+        })
+    },
+
+    beforeDestroy() {
+        this.editor.destroy()
+    },
+}
+</script>
